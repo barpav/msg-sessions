c89ee98f3cfe8b64c5bf5125ec97054e0bad7297
pavel <p.baranushkin@gmail.com>
2023-06-21 19:15:53 +0400

    fix: basic app structure (in progress)

--- internal/api/end.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func (s *Service) endSessions(w http.ResponseWriter, r *http.Request) {
-
-}
--- internal/api/get.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func (s *Service) getActiveSessions(w http.ResponseWriter, r *http.Request) {
-
-}
