563746bf97819852b9493e01b9f2c263c91ae427
pavel <p.baranushkin@gmail.com>
2023-07-20 18:37:18 +0400

    feat: grpc client for other microservices

---
+++ grpc_client/validate_session.go
@@ -0,0 +1,19 @@
+package sessions
+
+import (
+	"context"
+	"fmt"
+
+	ssgrpc "github.com/barpav/msg-sessions/sessions_service_go_grpc"
+)
+
+func (c *Client) ValidateSession(ctx context.Context, key, ip, agent string) (userId string, err error) {
+	var result *ssgrpc.ValidationResult
+	result, err = c.stub.Validate(ctx, &ssgrpc.SessionData{Key: key, Ip: ip, Agent: agent})
+
+	if err != nil {
+		return "", fmt.Errorf("Failed to validate session (service 'sessions' client): %w", err)
+	}
+
+	return result.User, nil
+}
